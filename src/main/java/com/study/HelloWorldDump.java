eb6011cbe2fc53e18890d28faaa1f21b1659c232
Xun Qiao <woqiaoxun@163.com>
2021-08-19 23:25:38 +0800

    add return statement

--- src/main/java/com/study/HelloWorldDump.java
+++ src/main/java/com/study/HelloWorldDump.java
@@ -9,7 +9,7 @@ public class HelloWorldDump {
     public static byte[] dump() {
         ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
         cw.visit(V1_8, ACC_PUBLIC | ACC_SUPER, "sample/HelloWorld", null, "java/long/Object", null);
-
+        return null;
     }
 
 }
